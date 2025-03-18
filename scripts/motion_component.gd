209f636a1463a20fafccfeef4984eb2a8945f4bf
Raii <143903432+raiinamoor@users.noreply.github.com>
2025-03-18 20:10:36 +0300

    Изменить способ вычисления пути в AcceleratedMotion

--- scripts/motion_component.gd
+++ scripts/motion_component.gd
@@ -8,8 +8,6 @@ extends Node
 
 @export var arrow: Node3D
 
-var prev_pos: Vector3 = Vector3.ZERO
-
 
 func _process(_delta: float) -> void:
 	var t: float = timer.elapsed_time_with_delay
@@ -27,14 +25,6 @@ func _process(_delta: float) -> void:
 		else:
 			scale_value = 0.1
 		arrow.scale.z = scale_value
-	
-	if motion is AcceleratedMotion:
-		var diff = target.global_position - prev_pos
-		motion.accumulated_distance += diff.length()
-		
-		prev_pos = target.global_position
-
 
 func set_state(new_state: Motion):
 	motion = new_state
-	prev_pos = Vector3.ZERO
--- scripts/motion_types/accelerated_motion.gd
+++ scripts/motion_types/accelerated_motion.gd
@@ -17,7 +17,42 @@ func _linear_velocity(t: float) -> Vector3:
 
 
 func _distance(t: float) -> float:
-	return accumulated_distance
-#func _distance(t: float) -> float:
-	#var displacement: Vector3 = initial_velocity * t + acceleration * t**2 / 2
-	#return displacement.length()
+	# путь вычисляется посредством поиска момента времени, в которой скорость меняет знак,
+	# и сложения абсолютных значений площадей под графиком скорости.
+	# выполняется для каждой оси координат
+	var area: float = 0 
+	
+	var area_x: float = 0
+	var t_0_x: float = (-initial_velocity.x) / acceleration.x	# момент времени, в который скорость равна 0
+	if acceleration.x == 0:
+		area_x = initial_velocity.x * t
+	else:
+		var t_interval1 = clampf(t, 0, t_0_x) 
+		var t_interval2 = clampf(t, t_0_x, t) 
+		var area_before_x = abs(_coordinate(t_interval1).x - _coordinate(0).x)
+		var area_after_x = abs(_coordinate(t_interval2).x - _coordinate(t_0_x).x)
+		area_x = area_before_x + area_after_x
+	
+	var area_y: float = 0
+	var t_0_y: float = (-initial_velocity.y) / acceleration.y	# момент времени, в который скорость равна 0
+	if acceleration.y == 0:
+		area_y = initial_velocity.y * t
+	else:
+		var t_interval1 = clampf(t, 0, t_0_y) 
+		var t_interval2 = clampf(t, t_0_y, t) 
+		var area_before_y = abs(_coordinate(t_interval1).y - _coordinate(0).y)
+		var area_after_y = abs(_coordinate(t_interval2).y - _coordinate(t_0_y).y)
+		area_y = area_before_y + area_after_y
+	
+	var area_z: float = 0
+	var t_0_z: float = (-initial_velocity.z) / acceleration.z	# момент времени, в который скорость равна 0
+	if acceleration.z == 0:
+		area_z = initial_velocity.z * t
+	else:
+		var t_interval1 = clampf(t, 0, t_0_z) 
+		var t_interval2 = clampf(t, t_0_z, t) 
+		var area_before_z = abs(_coordinate(t_interval1).z - _coordinate(0).z)
+		var area_after_z = abs(_coordinate(t_interval2).z - _coordinate(t_0_z).z)
+		area_z = area_before_z + area_after_z
+	
+	return sqrt(area_x**2 + area_y**2 + area_z**2)
